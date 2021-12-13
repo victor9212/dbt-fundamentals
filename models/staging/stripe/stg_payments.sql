cf3357a348172bac80742c863150e79f2fff618c
victor.yoshidi@cherre.com <victor.yoshidi@cherre.com>
2021-12-13 23:30:43 +0000

    Modularized code

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,11 @@
+with payments as (
+    SELECT 
+        id  as payment_id
+    ,   orderid as order_id
+    ,   paymentmethod as payment_method
+    ,   status
+    ,   amount / 100 as amount
+    ,   created as created_at
+    FROM `dbt-tutorial.stripe.payment`
+)
+SELECT * FROM payments  
